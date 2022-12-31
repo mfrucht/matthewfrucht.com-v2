fb2d967c28f3b58969d3ca59ac97d2ddf9bfb90a
Matthew Frucht <msf325@gmail.com>
2022-12-30 19:46:19 -0500

    fun(?) background placeholder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const images = ['./assets/images/backdrop3.png', './assets/images/backdrop4.png'];
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { imgUrl: images[1] };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.state.imgUrl === images[0] ? this.setState({ imgUrl: images[1] }) 
+        : this.setState({ imgUrl: images[0] })
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" style={{backgroundImage: `url(${this.state.imgUrl})`}}>
+          <p style={{color: 'black'}}>Under Construction</p>
+        </header>
+      </div >
+    );
+  }
 }
 
 export default App;
