58b46b3178c7876c772b6fdccf1bacc515936e84
Matthew Frucht <msf325@gmail.com>
2022-12-30 21:29:16 -0500

    removes unused files, edits ReadMe

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const images = ['./assets/images/backdrop1.png', './assets/images/backdrop2.png'];
+const images = [
+  './assets/images/backdrop1.png',
+  './assets/images/backdrop2.png',
+  './assets/images/backdrop3.png'];
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { imgUrl: images[1] };
+    this.state = { imgInd: 1 };
   }
 
   componentDidMount() {
     this.interval = setInterval(() => {
-      this.state.imgUrl === images[0] ? this.setState({ imgUrl: images[1] }) 
-        : this.setState({ imgUrl: images[0] })
+      this.state.imgInd === images.length - 1 ? this.setState({ imgInd: 0 }) 
+        : this.setState(prevState => { return { imgInd: prevState.imgInd+1 }})
     }, 1000);
   }
 
@@ -24,8 +27,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header" style={{backgroundImage: `url(${this.state.imgUrl})`}}>
-          <p style={{color: 'black'}}>Under Construction</p>
+        <header className="App-header" style={{ backgroundImage: `url(${images[this.state.imgInd]})`}}>
+          <p style={{color: 'black'}}>Matthew Frucht</p>
         </header>
       </div >
     );
